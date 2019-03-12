8eeed11f814547987d6508d20a0aa229d24de3c1
legacy-concept-8860 <miala0925@yahoo.com>
2019-03-12 02:42:05 +0000

    Automatically backed up by Learn

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -1,8 +1,8 @@
-puts 'A broken program' do
+puts "A broken program" do
 
-puts 'raises an error when loaded' do
+puts "raises an error when loaded" do
    expect{
-  puts 'lib/a_broken_program.rb'
+  puts "lib/a_broken_program.rb"
     }.to raise_error
     
 end
